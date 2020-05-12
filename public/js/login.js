3a7e639427ffe8443ffcb3f0bbb2b5d2f1dd88b9
Kisha Prudente <kisha.prudente@gmail.com>
2020-05-12 12:02:55 -0400

    fix lint

--- public/js/login.js
+++ public/js/login.js
@@ -1,41 +1,40 @@
 $(document).ready(function () {
-  // Getting references to our form and inputs
-  const loginForm = $("form.login");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+	// Getting references to our form and inputs
+	const loginForm = $("form.login");
+	const emailInput = $("input#email-input");
+	const passwordInput = $("input#password-input");
 
-  // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function (event) {
-    event.preventDefault();
-    console.log("click");
-    const userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim(),
-    };
-    console.log(userData);
+	// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+	async function loginUser(email, password) {
+		try {
+			await $.post("/api/login", {
+				email: email,
+				password: password,
+			});
+			window.location.replace("/members");
+		} catch (err) {
+			// If there's an error, log the error
+			console.log(err);
+		}
+	}
 
-    if (!userData.email || !userData.password) {
-      return;
-    }
+	// When the form is submitted, we validate there's an email and password entered
+	loginForm.on("submit", function (event) {
+		event.preventDefault();
+		console.log("click");
+		const userData = {
+			email: emailInput.val().trim(),
+			password: passwordInput.val().trim(),
+		};
+		console.log(userData);
 
-    // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
-  });
+		if (!userData.email || !userData.password) {
+			return;
+		}
 
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password) {
-    $.post("/api/login", {
-      email: email,
-      password: password,
-    })
-      .then(function () {
-        window.location.replace("/members");
-        // If there's an error, log the error
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
+		// If we have an email and password we run the loginUser function and clear the form
+		loginUser(userData.email, userData.password);
+		emailInput.val("");
+		passwordInput.val("");
+	});
 });
