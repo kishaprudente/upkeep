be12f22654d2190a03279b0890ce8d0dde821784
kwu3 <kwu3@villanova.edu>
2020-05-13 13:37:50 -0400

    Added axios and jest test packages, changed test command to use jest

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
-    database: "upkeep_db",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: 3306,
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+	development: {
+		username: "root",
+		password: process.env.DB_PASSWORD,
+		database: "upkeep_db",
+		host: "127.0.0.1",
+		dialect: "mysql",
+		port: 3306,
+	},
+	test: {
+		username: "root",
+		password: process.env.DB_PASSWORD,
+		database: "upkeep_test_db",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	production: {
+		username: "root",
+		password: null,
+		database: "database_production",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
 };
