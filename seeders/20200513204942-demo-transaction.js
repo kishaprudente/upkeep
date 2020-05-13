02dd7037ac0533ffeb9fd1f376164ed8fe5ed819
kwu3 <kwu3@villanova.edu>
2020-05-13 17:36:13 -0400

    Forced test seeder id values to be fixed values

--- seeders/20200513204942-demo-transaction.js
+++ seeders/20200513204942-demo-transaction.js
@@ -2,27 +2,25 @@
 "use strict";
 
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		const [firstUser] = await queryInterface.sequelize.query(
-			"SELECT id FROM Users"
-		);
-		const { id } = firstUser;
+	up: (queryInterface, Sequelize) => {
 		return queryInterface.bulkInsert(
 			"Transactions",
 			[
 				{
+					id: 1,
 					purpose: "Rent",
 					amount: "800.00",
 					note: "Rent for May",
-					UserId: id,
+					UserId: 1,
 					createdAt: new Date(),
 					updatedAt: new Date(),
 				},
 				{
+					id: 2,
 					purpose: "Food",
 					amount: "60.00",
 					note: "Pizza Party",
-					UserId: id,
+					UserId: 1,
 					createdAt: new Date(),
 					updatedAt: new Date(),
 				},
