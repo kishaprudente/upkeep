b1fbb7b6008ef2e81a121689585b9444f2bd0471
kwu3 <kwu3@villanova.edu>
2020-05-13 18:49:33 -0400

    Created CRUD tests for transactions

--- test/transaction-api-routes.test.js
+++ test/transaction-api-routes.test.js
@@ -47,4 +47,78 @@ describe("Transaction API Routes", () => {
 	});
 	// /api/transactions/:id
 	// get, post, delete, update request
+	describe("POST /api/transactions", () => {
+		it("should return a status code of 200", async () => {
+			const { status } = await axios.post("/api/transactions", {
+				id: 10000,
+				purpose: "Utilities",
+				amount: "500.00",
+				note: "Water & Electric & Gas",
+				UserId: 1,
+			});
+			expect(status).toBe(200);
+		});
+	});
+	describe("GET /api/transactions/:id where id = 10000 assuming test user 1 is logged in", () => {
+		it("should return a status code of 200", async () => {
+			const { status } = await axios.get("/api/transactions/10000");
+			expect(status).toBe(200);
+		});
+		it("should have content type json", async () => {
+			const expectedContentType = "application/json";
+			const { headers } = await axios.get("/api/transactions/10000");
+
+			const hasJSON =
+				JSON.stringify(headers).indexOf(expectedContentType) !== -1;
+
+			expect(hasJSON).toBe(true);
+		});
+		it("should return an array of one object if it's not empty", async () => {
+			const { data } = await axios.get("/api/transactions/10000");
+			const isObject = typeof data[0] === "object";
+
+			if (data.length > 0) {
+				expect(isObject).toBe(true);
+			} else {
+				expect(isObject).toBe(false);
+			}
+		});
+		it("should have the correct properties purpose and amount with existing values", async () => {
+			const { data } = await axios.get("/api/transactions/10000");
+			const isObject = typeof data[0] === "object";
+
+			if (data.length > 0) {
+				const [transaction] = data;
+				const { purpose, amount } = transaction;
+				expect(purpose).toBeTruthy();
+				expect(amount).toBeTruthy();
+			} else {
+				expect(isObject).toBe(false);
+			}
+		});
+	});
+	describe("PUT /api/transactions/10000", () => {
+		beforeEach(async () => {
+			jest.setTimeout(10000);
+		});
+		it("should return a status code of 200", async () => {
+			try {
+				const { status } = await axios.put("/api/transactions/3", {
+					purpose: "Utilities",
+					amount: "401.00",
+					note: "Water & Electric",
+				});
+				console.log(status);
+				expect(status).toBe(200);
+			} catch (err) {
+				throw err;
+			}
+		});
+	});
+	describe("DELETE /api/transactions/10000", () => {
+		it("should return a status code of 200", async () => {
+			const { status } = await axios.delete("/api/transactions/10000");
+			expect(status).toBe(200);
+		});
+	});
 });
