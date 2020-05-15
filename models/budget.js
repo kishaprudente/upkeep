d337e311cd3b4eccb62de95e7ed766db7291d055
Kisha Prudente <kisha.prudente@gmail.com>
2020-05-14 23:47:01 -0400

    added functions to get budgets

--- models/budget.js
+++ models/budget.js
@@ -1,13 +1,54 @@
 module.exports = function (sequelize, DataTypes) {
 	const Budget = sequelize.define("Budget", {
-		balance: {
+		total: {
 			type: DataTypes.DECIMAL,
 			allowNull: false,
 			validate: {
 				isNumeric: true,
 			},
 		},
-		month: DataTypes.DATE,
+		rent: {
+			type: DataTypes.DECIMAL,
+			allowNull: true,
+			validate: {
+				isNumeric: true,
+			},
+		},
+		food: {
+			type: DataTypes.DECIMAL,
+			allowNull: true,
+			validate: {
+				isNumeric: true,
+			},
+		},
+		utilities: {
+			type: DataTypes.DECIMAL,
+			allowNull: true,
+			validate: {
+				isNumeric: true,
+			},
+		},
+		savings: {
+			type: DataTypes.DECIMAL,
+			allowNull: true,
+			validate: {
+				isNumeric: true,
+			},
+		},
+		personal: {
+			type: DataTypes.DECIMAL,
+			allowNull: true,
+			validate: {
+				isNumeric: true,
+			},
+		},
+		miscellaneous: {
+			type: DataTypes.DECIMAL,
+			allowNull: true,
+			validate: {
+				isNumeric: true,
+			},
+		},
 	});
 
 	Budget.associate = function (models) {
