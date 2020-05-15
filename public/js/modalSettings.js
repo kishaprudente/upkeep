b25197d2ca5902f42171c42e3d6a985cd7951102
kwu3 <kwu3@villanova.edu>
2020-05-15 10:22:26 -0400

    Fixed add transaction functionality

--- public/js/modalSettings.js
+++ public/js/modalSettings.js
@@ -1,23 +1,25 @@
 const gear = $("#mySidenav");
 const plus = $("#mySide");
-const dropdown =$("#dropdown");
-const viewTransactions = $("#transactionSideButton")
+const dropdown = $("#dropdown");
+const viewTransactions = $("#transactionSideButton");
 
-plus.on('click touch', function (){
+plus.on("click touch", function () {
 	$("#modalTransaction").toggleClass("is-active");
 });
-gear.on('click touch', function () {
+gear.on("click touch", function () {
 	$("#modalCard").toggleClass("is-active");
 });
 
-viewTransactions.on('click touch', function () {
-	$("body, html").animate({
-        scrollTop: $(".transactions-list").offset().top
-	}, 1000);
+viewTransactions.on("click touch", function () {
+	$("body, html").animate(
+		{
+			scrollTop: $(".transactions-list").offset().top,
+		},
+		1000
+	);
 });
 
-
-dropdown.click(function (event){
+dropdown.click(function (event) {
 	event.stopPropagation();
 	$("#dropdown").toggleClass("is-active");
 });
